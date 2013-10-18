17d554be059035d135c2b0f95162961f0cbabf0c
janjakabovic <janyjakabovic@gmail.com>
2013-10-18 21:42:49 +0200

    v kope

---
+++ ajax.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+				$('a.ajax').click(function(){
+				$('#wrapper').fadeOut(0);
+				var xmlhttp;
+				if (window.XMLHttpRequest)
+				  {// code for IE7+, Firefox, Chrome, Opera, Safari
+				  xmlhttp=new XMLHttpRequest();
+				  }
+				else
+				  {// code for IE6, IE5
+				  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+				  }
+				xmlhttp.onreadystatechange=function()
+				  {
+				  if (xmlhttp.readyState==4)
+					{
+					xmlDoc=(xmlhttp.responseText);
+					var title=(/<title>([^<]+)<\/title>/m).exec(xmlDoc)[1];
+					document.title=title;
+					var wrapper=(/<div *(id=['"]wrapper['"][^<>]*|(?:[^<>=]+=['"][^'"]+['"] *)?id=['"]wrapper['"]) *>/m).exec(xmlDoc)[0];
+					var back1=(/<div *(id=['"]back1['"][^<>]*|(?:[^<>=]+=['"][^'"]+['"] *)?id=['"]back1['"]) *>/m).exec(xmlDoc)[0];
+					xmlDoc=xmlDoc.substring(xmlDoc.indexOf(wrapper),xmlDoc.indexOf(back1));
+					document.getElementById("wrapper").outerHTML=xmlDoc;
+					
+					if(/<script[^<>]*>/.test(xmlDoc))
+						{start=/<script[^<>]*>/.exec(xmlDoc)[0];
+						 end=/<\/script[^<>]*>/.exec(xmlDoc)[0];
+						 console.log(xmlDoc.substring(xmlDoc.indexOf(start)+start.length,xmlDoc.indexOf(end)));
+						 eval(xmlDoc.substring(xmlDoc.indexOf(start)+start.length,xmlDoc.indexOf(end)));
+						 $('#wrapper').fadeOut(0);
+						 $('#wrapper').fadeIn(500);
+						 history.pushState(stateObj, "page 2", "bar.html");
+						}
+					history.pushState({foo:'xxxx'}, 'stranka x', this.href);
+					}
+				  }
+				xmlhttp.open("GET", this.href ,true);
+				
+				xmlhttp.send();
+				return false;
+				});
+				
+
+});
